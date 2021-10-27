64b08a330d8e7220a2fdecedb8ae023bd54799fe
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-10-27 12:55:02 -0400

    adds form with student info fields and update button

--- src/views/StudentsEdit.vue
+++ src/views/StudentsEdit.vue
@@ -1,21 +1,64 @@
 <template>
   <div class="students-edit">
-    <h1>{{ currentStudent.name }}</h1>
+    <form v-on:submit.prevent="editStudentInfo()">
+      <h1>Edit Student</h1>
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+      </ul>
+      <div>
+        First Name:
+        <input type="text" v-model="currentStudent.first_name" />
+      </div>
+      <div>
+        Last Name:
+        <input type="text" v-model="currentStudent.last_name" />
+      </div>
+      <div>
+        Email:
+        <input type="text" v-model="currentStudent.email" />
+      </div>
+      <div>
+        Phone Number:
+        <input type="text" v-model="currentStudent.phone_number" />
+      </div>
+      <div>
+        Short Bio:
+        <input type="text" v-model="currentStudent.bio" />
+      </div>
+      <div>
+        LinkedIn Url
+        <input type="text" v-model="currentStudent.linkedin_url" />
+      </div>
+      <div>
+        Twitter Handle:
+        <input type="text" v-model="currentStudent.twitter_handle" />
+      </div>
+      <div>
+        Blog:
+        <input type="text" v-model="currentStudent.blog_url" />
+      </div>
+      <div>
+        GitHub:
+        <input type="text" v-model="currentStudent.github_url" />
+      </div>
+      <div><img :src="currentStudent.image_url" /></div>
+      <input type="submit" value="Update" />
+    </form>
   </div>
 </template>
 
 <style></style>
 
 <script>
-import axios from "axios";
+// import axios from "axios";
 export default {
   data: function () {
     return {
       currentStudent: {
         first_name: "Bob",
         last_name: "Smith",
-        image_url: "https://unsplash.com/photos/ZPswLOq0FHE",
-        email: "bobsmith@gmailcom",
+        image_url: "https://www.bobross.com/content/bob_ross_img.png",
+        email: "bobsmith@gmail.com",
         phone_number: "555 - 666 - 7777",
         bio: "Grew up in a working class family, and is the first of his family to pursue a higher education. He grinded his way to the top.",
         linkedin_url: "linkedin.com/bob",
@@ -27,10 +70,10 @@ export default {
     };
   },
   created: function () {
-    axios.get(`/students/${this.$route.params.id}`).then((response) => {
-      console.log(response.data);
-      this.currentStudent = response.data;
-    });
+    // axios.get(`/students/${this.$route.params.id}`).then((response) => {
+    // console.log(response.data);
+    // this.currentStudent = response.data;
+    // });
   },
   methods: {
     // editStudentInfo: function () {
